38c9bdeaa9a32f85d33818a23ced65d2081f69dd
KonstantinDenisov <omacs@mail.ru>
2022-08-14 13:01:16 +0300

    create SpawnPoint

--- Items/Assets/Scripts/GameManagersScripts/SpawnPoint.cs
+++ Items/Assets/Scripts/GameManagersScripts/SpawnPoint.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class SpawnPoint : SingletonMonoBehaviour<SpawnPoint>
 {
@@ -9,6 +11,38 @@ public class SpawnPoint : SingletonMonoBehaviour<SpawnPoint>
     [SerializeField] private ItemInfo[] _itemsInfoArrey;
     [Range(0f, 1f)]
     [SerializeField] private float _itemSpawnChance;
+    //private bool _wasSpawn;
+    [SerializeField] private float _startTimeSpawn;
+    [SerializeField] private float _multiplyTimeSpawn;
+    private float _currentTimeSpawn;
+    [SerializeField] private float _firstCoordinateRange;
+    [SerializeField] private float _secondCoordinateRange;
+
+    #endregion
+
+
+    #region Unity Lifecycle
+
+    private void Start()
+    {
+        _currentTimeSpawn = _startTimeSpawn;
+        InvokeRepeating("SpawnItems", _startTimeSpawn, _currentTimeSpawn);
+        
+        // _wasSpawn = false; 
+    }
+
+    /*
+        private void Update()
+        {
+            if (_wasSpawn)
+            {
+                return;
+            }
+    
+            SpawnItems();
+            _wasSpawn = true;
+        }
+    */
 
     #endregion
 
@@ -26,9 +60,9 @@ public class SpawnPoint : SingletonMonoBehaviour<SpawnPoint>
 
         int chanceSum = 0;
 
-        foreach (ItemInfo pickUpInfo in _itemsInfoArrey)
+        foreach (ItemInfo itemInfo in _itemsInfoArrey)
         {
-            chanceSum += pickUpInfo.SpawnChance;
+            chanceSum += itemInfo.SpawnChance;
         }
 
         int randomChance = Random.Range(0, chanceSum);
@@ -37,8 +71,8 @@ public class SpawnPoint : SingletonMonoBehaviour<SpawnPoint>
 
         for (int i = 0; i < _itemsInfoArrey.Length; i++)
         {
-            ItemInfo pickUpInfo = _itemsInfoArrey[i];
-            currentChance += pickUpInfo.SpawnChance;
+            ItemInfo itemInfo = _itemsInfoArrey[i];
+            currentChance += itemInfo.SpawnChance;
 
             if (currentChance >= randomChance)
             {
@@ -47,9 +81,22 @@ public class SpawnPoint : SingletonMonoBehaviour<SpawnPoint>
             }
         }
 
-        ItemsBase pickUpPrefab = _itemsInfoArrey[currentIndex].ItemPrefab;
-        Instantiate(pickUpPrefab, transform.position, Quaternion.identity);
+        ItemsBase itemPrefab = _itemsInfoArrey[currentIndex].ItemPrefab;
+        Instantiate(itemPrefab, new Vector3(Random.Range(_firstCoordinateRange, _secondCoordinateRange),transform.position.y,transform.position.z), Quaternion.identity);
+
+        // _wasSpawn = false;
+        _currentTimeSpawn *= _multiplyTimeSpawn;
     }
 
+    #endregion
+
+
+    #region PrivateMethods
+/*
+    private IEnumerator Wait()
+    {
+        yield return new WaitForSeconds(_currentTimeSpawn);
+    }
+*/
     #endregion
 }
---
+++ Items/Assets/Scripts/ItemsScripts/ScoreUpItem.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreUpItem : ItemsBase
+{
+    protected override void ApplyEffect(Collision2D col)
+    {
+        
+    }
+}
